79f78105e69e9b8000654bd8873e9c518940fad1
AMaraza <allymaraza@outlook.com>
2025-03-10 14:03:12 -0400

    Added working radio buttons to image carousel

--- index.js
+++ index.js
@@ -9,14 +9,55 @@ function nextImage(){
         case 1:
             currentImage.src = "images/tempimages/2.jpg";
             currentImageNum = 2;
+            document.getElementById("2").checked = true;
             break;
         case 2:
             currentImage.src = "images/tempimages/3.jpg";
             currentImageNum = 3;
+            document.getElementById("3").checked = true;
             break;
         case 3:
             currentImage.src = "images/tempimages/1.png";
             currentImageNum = 1;
+            document.getElementById("1").checked = true;
             break;
     }
 }
+
+function prevImage(){
+    switch (currentImageNum){
+        case 1:
+            currentImage.src = "images/tempimages/3.jpg";
+            currentImageNum = 3;
+            document.getElementById("3").checked = true;
+            break;
+        case 2:
+            currentImage.src = "images/tempimages/1.png";
+            currentImageNum = 1;
+            document.getElementById("1").checked = true;
+            break;
+        case 3:
+            currentImage.src = "images/tempimages/2.jpg";
+            currentImageNum = 2;
+            document.getElementById("2").checked = true;
+            break;
+    }
+}
+
+function changeImage(){
+    if (document.getElementById("1").checked){
+        currentImage.src = "images/tempimages/1.png";
+        currentImageNum = 1;
+    }
+    else if(document.getElementById("2").checked){
+        currentImage.src = "images/tempimages/2.jpg";
+        currentImageNum = 2;
+    }
+    else if(document.getElementById("3")){
+        currentImage.src = "images/tempimages/3.jpg";
+        currentImageNum = 3;
+    }
+}
+
+
+setInterval(nextImage, 5000);
